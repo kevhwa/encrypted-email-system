6589a4ce1ee2cee3c20d9ad63b590d78e09e995a
Kevin Hwang <jch2169@columbia.edu>
2020-12-08 16:38:31 -0500

    file directory script

--- create-tree.sh
+++ create-tree.sh
@@ -1,13 +1,33 @@
-#!bin/bash
+#!/bin/bash
 dir="$1"
 
-[ $# -eq 0] && { echo "Usage: $0 dir-name"; exit 1; }
+[ $# -eq 0 ] && { echo "Usage: $0 dir-name"; exit 1; }
 
-if [-d "$dir" -a ! -h "$dir" ]
+if [ -d "$dir" -a ! -h "$dir" ]
 then
+    echo "Error: $dir already exists in $(readlink -f ${dir})."
+else    
     mkdir "$1"
     cd "$1"
 
     mkdir client-dir
     mkdir rootca-dir
-    mkdir -p server-dir/ca
+    mkdir -p server-dir/ca server-dir/passwords
+
+fi
+
+# Creates user directories for server and users
+input=("addleness" "analects" "annalistic" "anthropomorphologically" "blepharosphincterectomy" "corector" "durwaun" "dysphasia" "encampment" "endoscopic" "exilic" "forfend" "gorbellied" "gushiness" "muermo" "neckar" "outmate" "outroll" "overrich" "philosophicotheological" "pockwood" "polypose" "refluxed" "reinsure" "repine" "scerne" "starshine" "unauthoritativeness" "unminced" "unrosed" "untranquil" "urushinic" "vegetocarbonaceous" "wamara" "whaledom")
+for i in ${input[@]}
+do 
+    (umask 077; mkdir -p client-dir/$i)
+    (umask 077; mkdir -p server-dir/mailboxes/$i)
+done
+
+j=0
+while read line;
+do
+  echo "$line"
+  echo "$line" > "./server-dir/passwords/${input[j]}.txt"
+  ((j++))
+done < ../original_hashed_pass.txt
