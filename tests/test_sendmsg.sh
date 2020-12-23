484ad350c7b080406a6176c44e55f9d4eeb565a4
Eric Jing <epj32@cornell.edu>
2020-12-23 13:15:59 -0500

    uncomment echo

--- tests/test_sendmsg.sh
+++ tests/test_sendmsg.sh
@@ -52,7 +52,7 @@ echo -e "\n**** 6. Test attempt to send a message to a user who doesn't exist an
 
 ./bin/sendmsg -f ./mailboxes/$USER/test.txt -r addleness this-user-doesnt-exist
 
-# echo -e "\n***** 7. Testing what happens when fed endless data file"
+echo -e "\n***** 7. Testing what happens when fed endless data file"
 mkfifo ./tests/msgpipe
 sh ./tests/endless_gen.sh > ./tests/msgpipe &
 ./bin/sendmsg -f ./tests/msgpipe -r $USER addleness polypose
